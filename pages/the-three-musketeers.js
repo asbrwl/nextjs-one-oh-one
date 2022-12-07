0059709de7e826ecf1d6274d2faf806c15e90126
Akhilesh Sabharwal <45665064+asbrwl@users.noreply.github.com>
2022-12-07 11:13:37 +0530

    Update the-three-musketeers.js

--- pages/the-three-musketeers.js
+++ pages/the-three-musketeers.js
@@ -29,7 +29,7 @@ export default function Page() {
 
         <h3>The Three Musketeers</h3>
         <p className={styles.storytext}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        A young man—we can sketch his portrait at a dash. Imagine to yourself a Don Quixote of eighteen; a Don Quixote without his corselet, without his coat of mail, without his cuisses; a Don Quixote clothed in a woolen doublet.
         </p>
         
       </main>
